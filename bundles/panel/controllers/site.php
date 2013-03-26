8c5d506020513035a87613f7ee5220f52072469b
Julien Tant <julien@ao6-labs.eu>
2013-03-26 23:59:24 +0100

    ajout de robustesse dans la modification d'utilisateurs

--- bundles/panel/controllers/site.php
+++ bundles/panel/controllers/site.php
@@ -17,7 +17,10 @@ class Panel_Site_Controller extends Base_Controller {
 
 	public function action_updateusers($user_id)
 	{
-		$user = User::find($user_id)->fill(Input::all())->save();
+		$user = User::find($user_id);
+		$user->accessible(array('username', 'email', 'verified', 'disabled', 'deleted'));
+		$user->fill(Input::all())
+			->save();
 
 
 	    return Redirect::to_action('panel::site@editusers', array($user_id))->with('success', '1');
