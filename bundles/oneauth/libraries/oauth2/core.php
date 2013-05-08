f8c11dd1c2504d7d73aaf2673c8ca34d8ac3fc5a
Julien Tant <julien@ao6-labs.eu>
2013-05-08 22:07:24 +0200

    Ajout d'un UA pour github

--- bundles/oneauth/libraries/oauth2/core.php
+++ bundles/oneauth/libraries/oauth2/core.php
@@ -27,11 +27,6 @@ class Core extends OAuth_Core
 		// The transfer must always be returned
 		$options[CURLOPT_RETURNTRANSFER] = TRUE;
 
-		// The transfer must always be returned
-		$options[CURLOPT_HTTPHEADER] = array(
-		    'User-Agent: laravel/france'
-		);
-
 		// Open a new remote connection
 		$remote = curl_init($url);
 
